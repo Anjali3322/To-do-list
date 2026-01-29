fc58c3a1671f63cde61202388634bc3eb92011c2
Anjali <sethanjali332@gmail.com>
2026-01-29 13:48:33 +0530

    update

--- script.js
+++ script.js
@@ -4,23 +4,41 @@ function addList(){
     // let checkbox = document.createElement("input")
     // checkbox.type = checkbox
     let list = document.getElementById("itemList")
+    if(input.value==="") 
+    //     {    //     alert("type something")    // }
+        return;
     // addNewList.appendChild("checkbox")
     list.appendChild(addNewList)
+    
     addNewList.textContent = document.getElementById("input").value
     document.getElementById("input").value = ""
-    deleteButton(addNewList)
-}
-function deleteButton(addNewList){
+
+    let delEditBtn = document.createElement("div")
+    addNewList.appendChild(delEditBtn)
+    deleteButton(delEditBtn)
+    editButton(delEditBtn)
+
+function deleteButton(delEditBtn){
     let deleteBtn = document.createElement("button")
     deleteBtn.textContent = "delete"
-    deleteBtn.style.cssText ="color:red; border:none; backgroundColor:transparent"
-    addNewList.appendChild(deleteBtn)
+    deleteBtn.style.cssText ="color:red; border:none; background:transparent"
+    delEditBtn.appendChild(deleteBtn)
     deleteBtn.onclick = function(){
     addNewList.remove()
     }
 }
+function editButton(delEditBtn){
+    let editBtn = document.createElement("button")
+    editBtn.textContent = "edit"
+    editBtn.style.cssText ="color:red; border:none; background:transparent"
+    delEditBtn.appendChild(editBtn)
+    editBtn.onclick = function(){
+    input.value = addNewList.innerText
+    }
+}
+}
 
-let compOne = document.createElement("p")
+// let compOne = document.createElement("p")
 // let idx = 0
 // for(p[0] of complete){
 //     p[0].innerText = `complete ${idx}`
@@ -32,3 +50,5 @@ let compOne = document.createElement("p")
 // complete.insertAdjacentElement("beforeend", compTwo)
 // compOne.textContent = "complete"
 // compTwo.textContent = "Uncomplete"
+
+
