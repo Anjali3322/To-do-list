825272a2a476b5744f5c875891715aebb300f1e8
Anjali <sethanjali332@gmail.com>
2026-04-02 12:33:04 +0530

    review-code

--- script.js
+++ script.js
@@ -7,40 +7,66 @@ function addList(){
         input.value = "";
         return;
     }
-    let addNewList = document.createElement("li")
-    let task = document.getElementById("itemList")
-    let taskContent = document.createElement("p")
     
+    // CREATE ELEMENT
+    let task = document.getElementById("itemList");
+        let addNewList = document.createElement("li");
+            let leftDiv = document.createElement("div");
+                let checkbox = document.createElement("input");
+                let taskContent = document.createElement("p");
+            let rightDiv = document.createElement("div");
+                let deleteBtn = document.createElement("button")
+                let editBtn = document.createElement("button")
+    
+
+    // APPEND ELEMENT
+
+    addNewList.appendChild(leftDiv);
+        checkbox.type = "checkbox";
+        leftDiv.appendChild(checkbox);
+        leftDiv.appendChild(taskContent);
+    addNewList.appendChild(rightDiv);
+        rightDiv.appendChild(deleteBtn);
+        rightDiv.appendChild(editBtn);
+    
+
     taskContent.innerText = input.value
     if(input.value==="") 
         return;
     task.appendChild(addNewList)
-    addNewList.appendChild(taskContent)
+    // addNewList.appendChild(taskContent)
     document.getElementById("input").value = ""
-     
 
-    let delEditBtn = document.createElement("div")
-    addNewList.appendChild(delEditBtn)
+    checkbox.onchange = function(){
+        if(checkbox.checked){
+            taskContent.style.cssText = "text-decoration : line-through solid red 2px";
+        }
+        else{
+            taskContent.style.cssText = "text-decoration : none"
+        }
+    }
+     
+    // let delEditBtn = document.createElement("div")
+    // addNewList.appendChild(delEditBtn)
     deleteButton(delEditBtn)
     editButton(taskContent)
 
 function deleteButton(delEditBtn){
-    let deleteBtn = document.createElement("button")
+
     deleteBtn.textContent = "delete"
-    deleteBtn.style.cssText ="color:red; border:none; background:transparent"
+    deleteBtn.style.cssText ="color:red; background:transparent; border: 2px solid red; padding:6px; border-radius:6px; cursor:pointer"
     delEditBtn.appendChild(deleteBtn)
     deleteBtn.onclick = function(){
     addNewList.remove()
     }
 }
 function editButton(taskContent){
-    let editBtn = document.createElement("button")
+    
     editBtn.textContent = "edit"
-    editBtn.style.cssText ="color:red; border:none; background:transparent"
+    editBtn.style.cssText ="color:green; background:transparent; border: 2px solid green; padding:6px; border-radius:6px; cursor:pointer"
     delEditBtn.appendChild(editBtn)
     editBtn.onclick = function(){
     input.value = taskContent.innerText
-    // addNewList.innerText = ""
     editTask = taskContent; 
     }
 }
